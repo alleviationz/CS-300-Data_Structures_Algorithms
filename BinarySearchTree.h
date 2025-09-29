9b2a4dd33e27ae11331f8e96142bf683dd55b729
Alec Novak <alleviationzero@gmail.com>
2025-09-28 20:17:56 -0500

    capstone enhancement

---
+++ BinarySearchTree.h
@@ -0,0 +1,161 @@
+#ifndef BinarySearchTree_H
+#define BinarySearchTree_H
+
+#include <iostream>
+#include <time.h>
+#include <string>
+#include <algorithm>
+#include <stdlib.h>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <utility>
+#include "Course.cpp"
+#include <chrono>
+
+//============================================================================
+// Binary Search Tree Storage
+//============================================================================
+
+template <typename T>
+class BinarySearchTree {
+private:
+    // Node struct to hold course (or type T) data
+    struct Node {
+        T data;
+        unique_ptr<Node> left = nullptr;
+        unique_ptr<Node> right = nullptr;
+
+        // Node constructor w/ data assigned, initialize left and right pointers to nullptrs for future use
+        explicit Node(const T& data) : data(data) {};   // move constructor
+    };
+
+public:
+    // BST header
+
+    // root node initialized to nullptr after constructor is called
+    unique_ptr<Node> root;
+    int nodeCount = 0;
+
+    // timer is based on your relative computer, results are variable
+    auto getSearchTime() {
+        string nullString;
+        auto timer_start = chrono::high_resolution_clock::now();
+        this->SearchHelper(root, nullString);
+        auto timer_end = chrono::high_resolution_clock::now();
+        chrono::duration<double, milli> duration = timer_end - timer_start;
+        return duration.count();
+    }
+
+    // size calc base BST size + (Node count * Node size)
+    int getSize() {
+        // reset nodeCount before iterating
+        this->nodeCount = 0;
+        this->getNodeCount(root);
+        return (this->nodeCount * sizeof(Node) + getSizeBSTMembers());
+    }
+
+    void Search(const string& dataId) {
+        this->SearchHelper(root, dataId);
+    };
+
+    void InOrderPrint() {
+        this->InOrderPrintHelper(root);
+    };
+
+    void Insert(const T& data) {
+        this->InsertHelper(root, data);
+    };
+
+private:
+
+    int getSizeBSTMembers() {
+        // only calc static class members/pointer sizes, functions variables are dynamically allocated/deallocated
+        return sizeof(this) + sizeof(nodeCount) + sizeof(root);
+    };
+
+    void getNodeCount(unique_ptr<Node>& node) {
+
+        // check is given node pointer is null
+        if (!node) {
+            return;
+        }
+
+        // recursively iterate left subtree (smaller data first)
+        getNodeCount(node->left);
+
+        // increase the node count (once for each node when reversing the left-side recursion, similar to InOrderPrint)
+        this->nodeCount++;
+
+        // recursively iterate right subtree (then larger data)
+        getNodeCount(node->right);
+    };
+
+    void setRoot(const T& data) {
+        root = make_unique<Node>(data);
+    };
+
+    // BST helper (logic processing) functions
+
+    T SearchHelper(unique_ptr<Node>& node, const string& dataId) {
+
+        // recurse until null node, searching BST by comparing Id values
+        if (node) {
+
+            // if node matches, return node's data object
+            if (dataId == node->data.courseId) {
+                cout << "Data returned: " << node->data.courseId << ": " << node->data.courseName << endl;
+                return node->data;
+            }
+            // if the data is less than node's dataId, recursively call the left tree and return recursive result
+            else if (dataId < node->data.courseId) {
+                return SearchHelper(node->left, dataId);
+            }
+            // else, dataId must be greater, so recursively call the right tree and return recursive result
+            else {
+                return SearchHelper(node->right, dataId);
+            }
+        }
+        // Data was not found, so return an empty Data object
+        T data;
+        return data;
+    };
+
+    void InOrderPrintHelper(unique_ptr<Node>& node) {
+        // check is given node pointer is null
+        if (!node) {
+            return;
+        }
+
+        // recursively iterate left subtree (smaller data first)
+        InOrderPrintHelper(node->left);
+
+        // output the Course ID for the node
+        cout << "Course ID: " << node->data.courseId << " Course Name: " << node->data.courseName << endl;
+
+        // recursively iterate right subtree (then larger data)
+        InOrderPrintHelper(node->right);
+    };
+
+    void InsertHelper(unique_ptr<Node>& node, const T& data) {
+
+        // if the  node is empty, set the node's data to the given data and end the process
+        if (!node) {
+            node = make_unique<Node>(data);
+            cout << "node set with course: " << node->data.courseId << endl;
+            return;
+        }
+
+        // find an empty node to insert data by recursing down subtrees until a node is null
+        if (node->data.courseId > data.courseId) {
+            // data is less than currNode data -> recurse left subtree
+            this->InsertHelper(node->left, data);
+        }
+        else {
+            // otherwise data is greater than currNode data -> recurse right subtree
+            this->InsertHelper(node->right, data);
+        }
+    };
+};
+
+#endif // BinarySearchTree_H
